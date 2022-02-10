1c139269616222ba65b67a0fa7baf43bbce79674
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-02-09 23:25:49 -0500

    Finished RWD for staying-in portion of website.

--- assets/js/staying-in.js
+++ assets/js/staying-in.js
@@ -1,16 +1,23 @@
-var apiKey =  "api_key=c07b2488e7306d0323e72d8fd4992d94";
-var movieInputEl = document.getElementById("id-search-input");
-var searchBtn = document.getElementById("id-search-btn");
-var movieFormEl= document.getElementById("id-search-form");
-var divContainer = document.getElementById("id-div-container");
+var apiKey =  'api_key=c07b2488e7306d0323e72d8fd4992d94';
+var movieInputEl = document.getElementById('id-search-input');
+var searchBtn = document.getElementById('id-search-btn');
+var movieFormEl= document.getElementById('id-search-form');
+var divContainer = document.getElementById('id-div-container');
+// div to hold movie poster and details about the movie, cast, runtime, etc
+var divContainerChild = document.createElement('div');
+divContainerChild.className = 'columns border-me-orange';
+// div to hold miscellaneous movie information on right-hand side
+var divMovieInformationList = document.createElement('div');
+divMovieInformationList.className = 'column';
+divMovieInformationList.setAttribute('id', 'id-movie-information-list')
 
-var baseUrl = "https://api.themoviedb.org/3" // base URL to which we will concatenate
-var imageBaseUrl = "https://image.tmdb.org/t/p/original" // for pulling movie posters
-var youtubeBaseUrl = "https://www.googleapis.com/youtube/v3/search"; // for pulling movie trailer
-var lang = "&language=en-US"; // used in multiple API fetches
+var baseUrl = 'https://api.themoviedb.org/3' // base URL to which we will concatenate
+var imageBaseUrl = 'https://image.tmdb.org/t/p/original' // for pulling movie posters
+var youtubeBaseUrl = 'https://www.googleapis.com/youtube/v3/search'; // for pulling movie trailer
+var lang = '&language=en-US'; // used in API fetches
 var searchResults = []; // hold movie search results. Array of objects
-var watchLink = "";
-var vidId = "";
+var watchLink = '';
+var vidId = '';
 
 
 var formSubmitHandler = function(event){
@@ -27,17 +34,17 @@ var formSubmitHandler = function(event){
         getSearchResults(searchString);
         displaySearchResults();
         // clear old content
-        movieInputEl.value = "";
+        movieInputEl.value = '';
     } else{
-        alert("Please enter a movie");
+        alert('Please enter a movie');
     }
 }
 
 // the first fetch for results; populates searchResults[] with movie objects
 var getSearchResults = function(string){
-    var includeAdult = "&include_adult=false"; // no adult titles returned
-    var searchMovie = "/search/movie?";
-    var apiUrl = baseUrl + searchMovie + apiKey + lang + "&query=" + string + includeAdult;
+    var includeAdult = '&include_adult=false';
+    var searchMovie = '/search/movie?';
+    var apiUrl = baseUrl + searchMovie + apiKey + lang + '&query=' + string + includeAdult;
      // a fetch returns a promise that resolves to the response to that request,
      // as soon as the server responds with headers, even if the server response is an HTTP error status.
     fetch(apiUrl)
@@ -48,12 +55,17 @@ var getSearchResults = function(string){
             for(var i = 0; i <data.results.length; i++){
                 searchResults.push(data.results[i])
             }
+            if(searchResults.length == 0){
+                var divBadSearch = document.createElement('div');
+                divBadSearch.innerHTML = `<h1 class='title is-2 text-color'>Your search returned with no results; please refine your search.</h1>`;
+                divContainer.append(divBadSearch); 
+            }
             console.log(searchResults);
         })
             } else {
                 console.log(`The fetch for ${apiUrl} was not successful`);
             }
-        });
+        })
 };
 
 
@@ -66,47 +78,50 @@ var displaySearchResults = function(){
         setTimeout(() => {
             // display as many movie posters as there are items in searchResults[]
             for(var i = 0; i < searchResults.length; i++){
+                
                 // create card container which will hold movie title, poster, and movie plot text
-                var movieCard = document.createElement("div");
-                movieCard.className = "card column is-3 mx-6 my-3 px-2 py-2 is-shadowless background-yellow";
+                var movieCard = document.createElement('div');
+                movieCard.className = `card column is-full-mobile is-half-tablet
+                 is-one-third-desktop is-one-fifth-widescreen movie-card mx-1 my-5 px-1 py-2 is-shadowless 
+                 card-background-transparent-blur`;
+                
                 // movie header holds movie title and favorites button
-                var movieHeader = document.createElement("div");
-                movieHeader.className = "card-header is-shadowless";
+                var movieHeader = document.createElement('div');
+                movieHeader.className = 'card-header is-shadowless';
                 
-                var movieTitle = document.createElement("span");
+                var movieTitle = document.createElement('div');
                 movieTitle.innerText = searchResults[i].original_title + ' - ' + searchResults[i].release_date.substring(0,4);
-                movieTitle.className = "card-header-title";
+                movieTitle.className = 'card-header-title title-text text-color';
                 
-                var addToFavBtn = document.createElement("button");
-                addToFavBtn.className = "card-header-icon";
-                addToFavBtn.setAttribute("data-movie-id", searchResults[i].id);
-                addToFavBtn.innerHTML = `<span class="icon-text has-text-info">
-                                            <span class="icon">
-                                                <i class="far fa-star"></i>
+                var addToFavBtn = document.createElement('button');
+                addToFavBtn.className = 'movie-add-fav card-header-icon';
+                addToFavBtn.setAttribute('data-movie-id', searchResults[i].id);
+                addToFavBtn.innerHTML = `<span class='icon-text has-text-success has-text-weight-medium'>
+                                            <span class='icon'>
+                                                <i class='far fa-star'></i>
                                             </span>
-                                            <span>Add To Favs</span>
-                                        </span>`; // <i class="fas fa-star"></i> for filled-in star
+                                            <span>Add</span>
+                                        </span>`;
                 addToFavBtn.addEventListener('click', (event) => {
                     amendFavoritesList(event);
                 });
                 
                 // div to hold poster and plot text
-                var divPosterAndPlot = document.createElement("div");
-                divPosterAndPlot.className = "display-it-flex max-height-div-container";
+                var divPosterAndPlot = document.createElement('div');
+                divPosterAndPlot.className = 'poster-and-plot is-flex is-justify-content-center max-height-div-container text-color';
                 
-                var moviePoster = document.createElement("div");
-                moviePoster.innerHTML = `<figure class="image is-2by3">                            
-                                            <img src="` + getMoviePosterImage(searchResults[i].id) + `" alt="Movie poster null">
+                var moviePoster = document.createElement('div');
+                moviePoster.innerHTML = `<figure class='image is-2by3'>                            
+                                            <img src='` + getMoviePosterImage(searchResults[i].id) + `' alt='Movie poster null'>
                                         </figure>`;
-                moviePoster.className = "card-image poster-size hand-pointer";
-                moviePoster.setAttribute("data-movie-id", searchResults[i].id);
+                moviePoster.className = 'poster card-image hand-pointer';
+                moviePoster.setAttribute('data-movie-id', searchResults[i].id);
                 // add event listener to movie poster such that when the poster is clicked, run displayMovieInformation
                 moviePoster.addEventListener('click', displayMovieInformation);
 
-                var moviePlot = document.createElement("p");
+                var moviePlot = document.createElement('div');
                 moviePlot.innerText = searchResults[i].overview;
-                moviePlot.className = `card-content
-                max-width-fifty-percent hide-overflow show-overflow-on-hover`;
+                moviePlot.className = 'plot handle-overflow card-content text-color';
     
                 // append elements to their respective containers
                 movieHeader.append(movieTitle, addToFavBtn);
@@ -132,7 +147,6 @@ var getMoviePosterImage = function(movieId){
     } 
 }
 
-
 var displayMovieInformation = function(){
     // clear out container because it will have all the card elements
     divContainer.replaceChildren();
@@ -148,10 +162,10 @@ var displayMovieInformation = function(){
 // update the icon styling as well
 var amendFavoritesList = function(event) {
     var clickedEl = event.target;
-    var nearestBtn = clickedEl.closest("button.card-header-icon");
+    var nearestBtn = clickedEl.closest('button.card-header-icon');
     if (nearestBtn != null) {
-        console.log("You clicked a movie to add to favorites!");
-        var favMovieId = nearestBtn.getAttribute("data-movie-id");
+        console.log('You clicked a movie to add to favorites!');
+        var favMovieId = nearestBtn.getAttribute('data-movie-id');
         var favMovieObject = searchResults.find(movieObject => movieObject.id == favMovieId);
         let currentFavorites = localStorage.getItem('favorites');
         if(nearestBtn.innerHTML.includes('fas')){
@@ -160,11 +174,11 @@ var amendFavoritesList = function(event) {
             return movieObject.id !== favMovieObject.id
             });
             localStorage.setItem('favorites', JSON.stringify(newFavorites));
-            nearestBtn.innerHTML = `<span class="icon-text has-text-info">
-                                        <span class="icon">
-                                            <i class="far fa-star"></i>
+            nearestBtn.innerHTML = `<span class='icon-text has-text-success has-text-weight-medium'>
+                                        <span class='icon'>
+                                            <i class='far fa-star'></i>
                                         </span>
-                                    <span>Add To Favs</span>
+                                    <span>Add</span>
                                     </span>`;
         } else {
             currentFavorites = currentFavorites ? JSON.parse(currentFavorites) : [];
@@ -174,16 +188,15 @@ var amendFavoritesList = function(event) {
             if (currentFavoriteIds.includes(favMovieObject.id)) return false;
             let newFavorites = JSON.stringify(currentFavorites.concat(favMovieObject))
             localStorage.setItem('favorites', newFavorites);
-            nearestBtn.innerHTML = `<span class="icon-text has-text-info">
-                                        <span class="icon">
-                                            <i class="fas fa-star"></i>
+            nearestBtn.innerHTML = `<span class='icon-text has-text-success has-text-weight-medium'>
+                                        <span class='icon'>
+                                            <i class='fas fa-star'></i>
                                         </span>
-                                        <span>Remove From List</span>
+                                        <span>Remove</span>
                                     </span>`;
         }
     } 
 }
-
 
 // fetch cast information from corresponding api endpoint
 // and also run getWatchProviders.
@@ -243,25 +256,38 @@ var getYoutubeVideoId = function(movieObjectIndex){
     '&type=video&maxResults=3&api_key=AIzaSyBoJlu4TN-eQm2gK0ce2uDgbO3RitGY-fQ';
 
     var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append('Content-Type', 'application/json');
     var requestOptions = {
-        method: "get",
+        method: 'get',
         headers: myHeaders,
-        redirect: "follow",  
+        redirect: 'follow',  
     };
 
     fetch(endpointForFetch, requestOptions)
         .then(response => response.json())
         .then(result => {
             vidId = result.items[0].id.videoId;
-            console.log('getYoutubeVideo(): Video id is ' + vidId);
-        //.catch(error => console.log('error', error))
+            console.log('getYoutubeVideoId(): Video id is ' + vidId);
+            displayMovieTrailer(vidId);
     });
+};
+
+// when the user clicks on a particular movie poster and the movie page loads,
+// display the movie trailer in divContainer after the miscellaneous movie information
+var displayMovieTrailer = function(vidId){
+    // div to hold iframe that shows the movie trailer
+    var movieIframe = document.createElement('iframe');
+    movieIframe.setAttribute('src', `https://www.youtube.com/embed/` + vidId);
+    console.log(movieIframe.getAttribute('src'));
+    movieIframe.setAttribute('width', '560', 'height', '515', 'target', '_parent', 'frameborder', '0', 'allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture', 'allowfullscreen');
+    movieIframe.className = 'column border-me-yellow';
+    // append iframe to divMovieInformationList
+    divMovieInformationList.append(movieIframe);
 };
 
 // gets the link URL for where the movie can be watched as well as dynamically 
 // creates HTML to populate the page after the user has clicked on a movie poster
-var getWatchProviders = function (movieId, movieObjectIndex, castString, vidId){
+var getWatchProviders = function (movieId, movieObjectIndex, castString){
     console.log('inside getWatchProviders()');
     var watchProviders = `/movie/${movieId}/watch/providers?`;
     var apiUrl = baseUrl + watchProviders + apiKey;
@@ -271,22 +297,20 @@ var getWatchProviders = function (movieId, movieObjectIndex, castString, vidId){
             if(response.ok){
                 response.json()
         .then(data => {
-            watchLink = data.results["US"].link;
-            // div to hold movie poster and details about the movie, cast, runtime, etc
-            var divContainerChild1 = document.createElement('div');
-            divContainerChild1.className = "columns";
-            // div to hold iframe that shows the movie trailer
-            var divContainerChild2 = document.createElement('div');
-            divContainerChild2.className = "columns div-container-child-2-min-height";
+            watchLink = data.results['US'].link;
+            // // div to hold movie poster and details about the movie, cast, runtime, etc
+            // var divContainerChild = document.createElement('div');
+            // divContainerChild.className = 'columns';
+            
             // div to hold large movie poster image
             var divMoviePosterImage = document.createElement('div');
-            divMoviePosterImage.className = "column is-one-third";
-            divMoviePosterImage.innerHTML = `<figure class="image is-2by3">                            
-                                                <img src="` + getMoviePosterImage(movieId) + `" alt="Movie poster null">
+            divMoviePosterImage.className = 'column is-one-third';
+            divMoviePosterImage.innerHTML = `<figure class='image is-2by3'>                            
+                                                <img src='` + getMoviePosterImage(movieId) + `' alt='Movie poster null'>
                                             </figure>`;
-            // div to hold miscellaneous movie information on right-hand side
-            var divMovieInformationList = document.createElement('div');
-            divMovieInformationList.className = "column";
+            // // div to hold miscellaneous movie information on right-hand side
+            // var divMovieInformationList = document.createElement('div');
+            // divMovieInformationList.className = 'column';
             
             // divs that are classes as Bulma block and that hold the miscellaneous movie info
             // i.e. title, synopsis, release date, cast, and link to where the movie can be watched
@@ -304,24 +328,11 @@ var getWatchProviders = function (movieId, movieObjectIndex, castString, vidId){
             blockMovieCast.innerText = 'Cast Includes: ' + castString;
             var blockWatchProviders = document.createElement('div');
             blockWatchProviders.className = 'block text-color';
-            blockWatchProviders.innerHTML = `For a link to watch providers for this film, click <a href="` + watchLink + `">here</a>.`;
-
-            // this iframe part is a bit wonky; needs improvement.
-            setTimeout(() => {
-                var movieIframe = document.createElement('iframe');
-                movieIframe.setAttribute("src", `https://www.youtube.com/embed/` + vidId);
-                console.log(movieIframe.getAttribute("src"));
-                movieIframe.setAttribute("width", "1120", "height", "630", "target", "_parent", "frameborder", "0", "allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture", "allowfullscreen");
-                movieIframe.className = "column";
-                // append iframe to divContainerChild2
-                divContainerChild2.append(movieIframe);
-                divContainer.append(divContainerChild2);
-            }, 2500);
-
+            blockWatchProviders.innerHTML = `For a link to watch providers for this film, click <a href='` + watchLink + `'>here</a>.`;
             // do the necessary appends, and finally append everything to divContainer
-            divMovieInformationList.append(blockMovieTitle, blockMoviePlot, blockReleaseDate, blockMovieCast, blockWatchProviders);
-            divContainerChild1.append(divMoviePosterImage, divMovieInformationList);
-            divContainer.append(divContainerChild1);
+            divMovieInformationList.prepend(blockMovieTitle, blockMoviePlot, blockReleaseDate, blockMovieCast, blockWatchProviders);
+            divContainerChild.append(divMoviePosterImage, divMovieInformationList);
+            divContainer.append(divContainerChild);
         })
             } else {
                 console.log(`The fetch for ${apiUrl} was not successful`);
@@ -332,7 +343,7 @@ var getWatchProviders = function (movieId, movieObjectIndex, castString, vidId){
 
 ///// Event Listeners /////
 // listen for click on search form and run formSubmitHandler
-movieFormEl.addEventListener("submit", formSubmitHandler);
+movieFormEl.addEventListener('submit', formSubmitHandler);
 
 // click listener for adding movie to favorites list
 divContainer.addEventListener('click', amendFavoritesList);
